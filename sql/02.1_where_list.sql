9f3a13aca8d5cfc11eb73aa7bfb9ed48fe4b6740
Mateus Musial <mateus.musial@gmail.com>
2022-12-27 10:27:57 -0300

    Add query search some rows

--- sql/02.1_where_list.sql
+++ sql/02.1_where_list.sql
@@ -2,3 +2,15 @@
 select *
   from employees emp
  where instr(nvl(:p_emp_id, emp.employee_id), emp.employee_id) > 0
+
+-- Query para buscar apenas algumas linhas
+select emp.*
+     , dep.*
+  from employees emp
+     , departments dep
+ where 1 = 1
+   and emp.department_id = dep.department_id
+   and rownum <= 5;
+
+select *
+  from employees fetch first 5 rows only;
